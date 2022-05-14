f79143269e7851dc85b14cba5063fb7f57ee9d45
Josiel <jdlopez114@gmail.com>
2022-05-14 17:17:09 -0400

    Code clean up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const firstGen = 151;
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchAllPokemon();
-    // activateModal();
 })
 
 const fetchAllPokemon = () => {
@@ -45,101 +44,9 @@ const fetchAllPokemon = () => {
                 modal.style.display = "none";
             }
 
-            // pokemonImage.addEventListener('click', () => {
-            //     modal.showModal();
-            // })
         })  
     }
 }
 
-const activateModal = (poke) => {
-    
-    let close = document.querySelector(".close");   
-                 
-    poke.onclick = () => {
-    poke.modal.style.display = "block";
-    poke.modalImage.src = pokemonData.sprites.back_default;
-    }
-
-    close.onclick = () => {
-        modal.style.display = "none";
-    }
-}
-
-// const closeModal = () => {
-   
-// }
 
 
-// function renderPokemon(pokemon){
-    
-//     pokemon.forEach(poke => {
-
-//             let pokemonContainer = document.querySelector('#pokemon-container');
-
-//             let pokemonCard = document.createElement('div');
-//             pokemonCard.classList.add("pokemon-card")
-//             let pokemonImage = document.createElement('img');
-//             pokemonImage.classList.add("pokemon-image")
-//             let pokemonNumber = document.createElement('h2');
-//             pokemonNumber.classList.add("pokemon-number")
-//             let pokemonName = document.createElement('h3');
-//             pokemonName.classList.add("pokemon-name")
-
-//             pokemonNumber.innerText = poke.id;
-//             pokemonName.innerText = poke.name[0].toUpperCase() + pokemonData.name.slice(1);
-//             pokemonImage.src = pokemonData.sprites.front_default;
-
-//             pokemonCard.append(pokemonImage, pokemonNumber, pokemonName);
-//             pokemonContainer.append(pokemonCard);
-//     })
-// }
-
-
-// const fetchPokemon = () => {
-//     const inputForm = document.querySelector('form');
-
-//     inputForm.addEventListener('submit', (event) => {
-//         event.preventDefault();
-//         const input = document.querySelector('input#searchByName');
-
-//         fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
-//         .then(res => res.json())
-//         .then(pokemonData => {
-//         // Object.keys(pokemonData).forEach((pokemon) => {
-//         //     addOnePokemon(pokemon);
-
-//         // });
-//         let pokemonContainer = document.querySelector('#pokemon-container');
-
-//         let card = document.createElement('div');
-//         let image = document.createElement('img');
-//         let id = document.createElement('h2');
-//         let name = document.createElement('h2');
-        
-//         id.innerText = pokemonData.id;
-//         name.innerText = pokemonData.name[0].toUpperCase() + pokemonData.name.slice(1);
-//         image.src = pokemonData.sprites.front_default;
-
-//         card.append(image, id, name);
-//         pokemonContainer.append(card);
-
-//         })
-//     });
-// }
-
-// const addOnePokemon = (pokemon) => {
-//     let pokemonContainer = document.querySelector('#pokemon-container');
-
-//     let card = document.createElement('div');
-//     let id = document.createElement('h2');
-//     let name = document.createElement('h2');
-//     // let type = document.createElement('h2');
-//     // let marketValue = document.createElement('h2');
-//     id.innerText = pokemon.id;
-//     name.innerText = pokemon.name;
-//     // type.innerText = pokemon.type;
-
-//     card.append(id, name);
-//     pokemonContainer.append(card);
-// }
